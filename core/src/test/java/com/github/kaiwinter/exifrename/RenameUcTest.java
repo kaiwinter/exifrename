4fe9eec242af8c1052892f6fe6e30589758023a4
Kai Winter <kaiwinter@gmx.de>
2020-10-24 15:41:53 +0200

    Fixed deprecated import

--- core/src/test/java/com/github/kaiwinter/exifrename/RenameUcTest.java
+++ core/src/test/java/com/github/kaiwinter/exifrename/RenameUcTest.java
@@ -1,6 +1,6 @@
 package com.github.kaiwinter.exifrename;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
